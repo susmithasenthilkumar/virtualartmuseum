74d7b03ca8d389ea5a84fd2b608d69868c194592
Susmitha G S <susmithasenthil01@gmail.com>
2026-09-22 23:02:57 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS ? '/virtualartmuseum/' : '/',
     plugins: [
       react(), 
       tailwindcss(),
